50d382f157a38ac5ae193f65184fd66fad821661
CodeWithGoyal <80843296+CodeWithGoyal@users.noreply.github.com>
2022-07-14 16:20:32 +0530

    Added solution - LeetHub

---
+++ Enemy - GFG/enemy.java
@@ -0,0 +1,123 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+        
+        return a;
+    }
+    
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+    
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+
+class IntMatrix
+{
+    public static int[][] input(BufferedReader br, int n, int m) throws IOException
+    {
+        int[][] mat = new int[n][];
+        
+        for(int i = 0; i < n; i++)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            mat[i] = new int[s.length];
+            for(int j = 0; j < s.length; j++)
+                mat[i][j] = Integer.parseInt(s[j]);
+        }
+        
+        return mat;
+    }
+    
+    public static void print(int[][] m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+    
+    public static void print(ArrayList<ArrayList<Integer>> m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int[] a = IntArray.input(br, 3);
+            int n = a[0],m=a[1],k=a[2];
+            
+            int[][] e = IntMatrix.input(br, a[2], 2);
+            
+            Solution obj = new Solution();
+            int res = obj.largestArea(n,m,k,e);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public static int largestArea(int n,int m,int k,int[][] e) {
+        // code here
+        int row[] = new int[k];
+        int col[] = new int[k];
+        for(int i = 0; i < k; i++){
+            row[i] = e[i][0]-1;
+            col[i] = e[i][1]-1;
+        }
+        Arrays.sort(row);
+        Arrays.sort(col);
+        int prev = -1,max = 0;
+        for(int i : row){
+            max = Math.max(max,i - prev - 1);
+            prev = i;
+        }
+        max = Math.max(max,n - prev - 1);
+        
+        prev = -1;
+        int ma = 0;
+        for(int i : col){
+            ma = Math.max(ma,i - prev - 1);
+            prev = i;
+        }
+        ma = Math.max(ma,m - prev - 1);
+        return ma*max;
+    }
+}
+        
