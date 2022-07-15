6fbf51cb31f3229825db39caa3b69907fad64fac
CodeWithGoyal <80843296+CodeWithGoyal@users.noreply.github.com>
2022-07-15 15:21:34 +0530

    Added solution - LeetHub

---
+++ Min Number of Flips - GFG/min-number-of-flips.java
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            String S = br.readLine().trim();
+            Solution ob = new Solution();
+            
+            System.out.println(ob.minFlips(S));
+                        
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    public int minFlips(String S) {
+        // Code here
+        int x = 0;
+        for(int i = 0; i < S.length(); i++){
+            if(i % 2 == 0 && S.charAt(i) != '0'){
+                x++;
+            }
+            else if(i % 2 != 0 && S.charAt(i) != '1') x++;
+        }
+        return Math.min(S.length() - x, x);
+    }
+}
