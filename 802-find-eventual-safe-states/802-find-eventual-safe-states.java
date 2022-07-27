d03e8f89406d672ec9455bdc9c008663ced351b2
CodeWithGoyal <80843296+CodeWithGoyal@users.noreply.github.com>
2022-07-27 23:10:12 +0530

    Time: 55 ms (26.30%), Space: 73.9 MB (28.63%) - LeetHub

---
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.java
@@ -0,0 +1,40 @@
+class Solution {
+    private boolean dfs(ArrayList<ArrayList<Integer>> adj, int visited[], List<Integer> res, int i){
+        if(visited[i] == -1) return false;
+        if(visited[i ] == 1) return true;
+        if(visited[i] == -2) return false;
+        if(adj.get(i).size() == 0){
+            visited[i] = 1;
+            res.add(i);
+            return true;
+        }
+        visited[i] = -2;
+        boolean re = true;
+        for(int x : adj.get(i)){
+            re = re & dfs(adj,visited,res,x);
+            // System.out.println(re + " " + x);
+        }
+        if(re) { visited[i] = 1;res.add(i);}
+        else visited[i] = -1;
+        
+        return visited[i] == 1;
+        
+    }
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> res = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < graph.length; i++){
+            adj.add(new ArrayList<>());
+            for(int x : graph[i])
+                adj.get(i).add(x);
+        }
+       int visited[] = new int[graph.length];
+        for(int i = 0;i < graph.length; i++){
+            if(visited[i] == 0){
+                dfs(adj,visited,res,i);
+            }
+        }
+        Collections.sort(res);
+        return res;
+    }
+}
